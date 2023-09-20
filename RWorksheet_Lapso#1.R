18708746b91585bdb6da29e3c916e2bdeaf13632
Darlene Erl Lapso <darlene7703@gmail.com>
2023-09-20 02:22:15 +0000

    kerenet nga 34

---
+++ RWorksheet_Lapso#1.R
@@ -0,0 +1,105 @@
+############1st activity
+ageVector <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 42, 51, 35, 24, 33, 41)
+
+
+#max
+max(ageVector) 
+
+#min
+min(ageVector)
+
+
+#seq
+seq(ageVector) 
+
+#sort
+sort(ageVector) 
+
+
+#assign    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+assign(ageVector) 
+
+#data points
+length(ageVector)
+
+
+#sequence
+seq(17, 57)
+
+#reciprocals(decimals)
+recip_age <- 1/ageVector
+recip_age
+
+
+
+#######2222222222nd activity
+dataVector <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.4, 2.7)
+
+#data points
+length(dataVector)
+
+#doubles the data
+dob_data <- 2*dataVector
+dob_data
+
+#int(1-100)
+seq(1, 100)
+length(seq)
+one_hundred <- seq
+
+
+#numbers(20-60)
+seq(20, 60, 10)
+twen_six <- seq
+length(twen_six)
+mean(20, 60)
+
+
+#sum(51-91)
+seq(51, 91)
+five_nine <- seq
+length(five_nine)
+sum(seq)
+
+#integers(1-1000)
+seq(1, 1000)
+
+#data point(8.1-8.4)
+
+
+
+
+
+
+#data points(8.5-10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
